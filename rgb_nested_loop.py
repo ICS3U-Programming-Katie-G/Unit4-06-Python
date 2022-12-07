bb5207e0b1db1d2dd4fcb3279fac3de7325f7250
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-12-07 20:47:36 +0000

    python code for the RGB color loop program

--- rgb_nested_loop.py
+++ rgb_nested_loop.py
@@ -2,29 +2,66 @@
 # Created by: Katie G
 # Created on: December 5th, 2022
 # This program will list every value of RGB.
-# The program uses 3 nested FOR... loops
+# The program uses 3 nested while... loops
 # to accomplish this task.
 
 
 # this function will display all of the respective R, G and B
-# values respectively using 3 nested FOR... loops.
+# values respectively using 3 nested while... loops.
 def main():
     # tell the user to look at all the RGB values
     # that are about to be printed.
     print("I really like RGB values! Look at all these colors :)")
 
-    # initializing the three counters.
+    # initializing the three counters. counter1 represents red,
+    # counter2 represents green, and counter3 represents blue.
     counter1 = 0
     counter2 = 0
     counter3 = 0
-    # first FOR... loop, containing the counter
-    # for the first value in RGB, R.
-    for counter1 in range(0, 256):
-        for counter2 in range(0, 256):
-            for counter3 in range(0, 256):
+    # first while... loop, containing the counter
+    # for the first value in RGB, R / red.
+    while counter1 <= 255:
+        print(
+                "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(
+                    str(counter1),
+                    str(counter2),
+                    str(counter3),
+                    "RGB("
+                    + str(counter1)
+                    + ","
+                    + str(counter2)
+                    + ","
+                    + str(counter3)
+                    + ")",
+                    )
+            )
+        if counter2 and counter3 == 255:
+            counter1 = counter1 + 1
+        # second while...loop, containing the counter
+        # for the second value in RGB, G / green.
+        while counter2 < 255:
+            print(
+                "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(
+                    str(counter1),
+                    str(counter2),
+                    str(counter3),
+                    "RGB("
+                    + str(counter1)
+                    + ","
+                    + str(counter2)
+                    + ","
+                    + str(counter3)
+                    + ")",
+                    )
+            )
+            if counter3 == 255:
+                counter2 = counter2 + 1
+            # third while...loop, containing the counter
+            # for the third value in RGB, B / blue.
+            while counter3 < 255:
                 print(
                     "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(
-                        str(counter1),
+                    str(counter1),
                         str(counter2),
                         str(counter3),
                         "RGB("
@@ -36,6 +73,7 @@ def main():
                         + ")",
                     )
                 )
+                counter3 = counter3 + 1
 
 
 if __name__ == "__main__":
